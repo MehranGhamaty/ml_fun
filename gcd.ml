dfaa357f67c774fc13d27735b378edcfe30599c8
Mehran Ghamaty <mnghamaty@gmail.com>
2017-01-05 23:52:17 -0800

    fixed gcd (again)

--- gcd.ml
+++ gcd.ml
@@ -1,18 +1,18 @@
 (* Euclid's *)
 fun gcd (a, b) = 
   let
-    fun reduce (a, 0) = a
-    |   reduce (0, b) = b
-    |   reduce (a, b) = if a = b then a else reduce (a-b, b)
+    fun reduce (0, b) = b
+    |   reduce (a, b) = reduce (b mod a, a)
   in
-    if a > b then reduce(a, b) else reduce (b, a)
+    if a < b then reduce(a, b) else reduce (b, a)
   end;
 
 gcd(30,60);
 gcd(60,30);
+gcd(160,12);
 
 fun lcd (0, b) = b
 |   lcd (a, 0) = a
 |   lcd (a, b) = if a > b then lcd (a-b, b) else lcd (a, b-a);
 
-lcd(2, 6);
+lcd(4, 6);
